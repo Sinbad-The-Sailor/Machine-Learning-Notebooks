acbd3de7aa105e1653093179e28687bef4282ec2
Axel Nilsson <Karlaxel.n@outlook.com>
2022-06-07 20:04:15 +0200

    Working data unpacker.

--- mnist_utils/data_unpacker.py
+++ mnist_utils/data_unpacker.py
@@ -1,1 +1,11 @@
-import gzip
+import os
+import patoolib
+
+# File path to data.
+DATA_PATH = os.getcwd() + "/../Data/"
+
+files = os.listdir(DATA_PATH)
+files = [DATA_PATH + file for file in files if file[-3:] == ".gz"]
+
+for file in files:
+    patoolib.extract_archive(file, outdir=DATA_PATH)
